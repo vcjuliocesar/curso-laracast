f892b87214c3db94e47a163fd5998086cffe5c18
julio cesar <vcjuliocesar@outlook.com>
2020-03-24 14:01:43 -0600

    route wildcards

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('test', function () {
     return view('test',['name'=>request('name')]);
+});*/
+
+Route::get('/posts/{post}', function ($post) {
+  $posts=[
+    'my-first-post'=>'hello this is my first post',
+    'my-second-post'=>'hello this is my second post',
+  ];
+
+  if(!array_key_exists($post, $posts)){
+     abort(404, 'message');
+  }
+    return view('post',['post'=>$posts[$post]]);
 });
