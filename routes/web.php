e877d5bd369c955bc75fbc33c6d3a7c9a08b32e6
julio cesar <vcjuliocesar@outlook.com>
2020-03-24 19:00:30 -0600

    add template

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,6 @@ Route::get('/',function(){
   return view('welcome');
 });
 
-Route::get('/contact',function(){
-  return view('contact');
+Route::get('/about',function(){
+  return view('about');
 });
